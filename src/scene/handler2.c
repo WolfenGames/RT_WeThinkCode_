5fa35d77b07e50277d863a461bc364b377092534
Julian Wolf <julian.w16@gmail.com>
2018-09-05 09:32:51 +0200

    add

--- src/scene/handler2.c
+++ src/scene/handler2.c
@@ -6,7 +6,7 @@
 /*   By: jwolf <jwolf@42.FR>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 07:51:39 by jwolf             #+#    #+#             */
-/*   Updated: 2018/09/05 09:24:56 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/09/05 09:32:44 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	set_tex(t_obj *o, char *filename, t_env *env)
 		if ((fd = open(filename, O_RDONLY)) < 0)
 			return ;
 		close(fd);
+		printf("Test\n");
 		o->tex = find_img(REN, add_img_xpm(REN, filename, 0, 0));
 	}
 }
