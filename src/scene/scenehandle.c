83c7e433cec88b61d071a4fec2c792f52d00f41b
Julian Wolf <julian.w16@gmail.com>
2018-09-13 11:56:44 +0200

    added scene

--- src/scene/scenehandle.c
+++ src/scene/scenehandle.c
@@ -6,7 +6,7 @@
 /*   By: jwolf <jwolf@42.FR>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 09:01:47 by jwolf             #+#    #+#             */
-/*   Updated: 2018/09/10 09:03:02 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/09/13 11:54:57 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 void	do_da_scene(char *line, t_scene *scene)
 {
 	if (match_brackets("raydepth", line))
-		scene->raydepth = ft_atod(line + 10);		
+		scene->raydepth = ft_atod(line + 10);
+	if (match_brackets("width", line))
+		scene->win_w = ft_atod(7);
+	if (match_brackets("height", line))
+		scene->win_h = ft_atod(8);
 }
