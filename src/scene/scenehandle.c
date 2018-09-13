643f700ca43af6b22e4a58f120624579d14bffca
Julian Wolf <julian.w16@gmail.com>
2018-09-13 12:18:57 +0200

    added fix..

--- src/scene/scenehandle.c
+++ src/scene/scenehandle.c
@@ -6,7 +6,7 @@
 /*   By: jwolf <jwolf@42.FR>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 09:01:47 by jwolf             #+#    #+#             */
-/*   Updated: 2018/09/13 11:54:57 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/09/13 12:18:47 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	do_da_scene(char *line, t_scene *scene)
 	if (match_brackets("raydepth", line))
 		scene->raydepth = ft_atod(line + 10);
 	if (match_brackets("width", line))
-		scene->win_w = ft_atod(7);
+		scene->win_w = ft_atoi(line + 7);
 	if (match_brackets("height", line))
-		scene->win_h = ft_atod(8);
+		scene->win_h = ft_atoi(line + 8);
 }
