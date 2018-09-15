b0d2570430fdc21d30cd7981df02324d59bcc57b
Isard BOTHA <ibotha@e4r15p6.wethinkcode.co.za>
2018-09-15 13:35:13 +0200

    General fix part two: What the Fix??

--- Vectorlib/refract.c
+++ Vectorlib/refract.c
@@ -6,32 +6,49 @@
 /*   By: ibotha <ibotha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 12:24:29 by ibotha            #+#    #+#             */
-/*   Updated: 2018/09/11 14:24:42 by ibotha           ###   ########.fr       */
+/*   Updated: 2018/09/15 13:34:28 by ibotha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vectorlib.h"
 
+static void	refract_helper(t_vec norm, t_vec tnorm, double var[6])
+{
+    var[1] = 1;
+    if (var[3] < 0)
+	{
+		var[3] = -var[3];
+		vec_dup(norm, tnorm);
+	}
+	else
+	{
+		ft_swap(&var[1], &var[2], sizeof(double));
+		v_multi(norm, -1, tnorm);
+	}
+	var[4] = var[1] / var[2];
+    var[5] = 1 - var[4] * var[4] * (1 - var[3] * var[3]); 
+}
+
 double	*refract(t_vec inc, t_vec norm, double index, t_vec ret)
 {
 	t_vec	tnorm;
-    double	NdotI = dot(norm, inc);
-    double	etai = 1;
-	double	etat = index;
+	double	var[6];
+
+    var[0] = dot(norm, inc);
+    var[1] = 1;
+	var[2] = index;
 	vec_dup(norm, tnorm);
-    if (NdotI < 0) 
-        NdotI = -NdotI;
+    if (var[0] < 0) 
+        var[0] = -var[0];
     else
 	{
         v_multi(norm, -1, tnorm);
-        ft_swap(&etai, &etat, sizeof(double));
+        ft_swap(&var[1], &var[2], sizeof(double));
 	}
-	double cosi = ft_clamp(1, -1, dot(inc, norm));
-    etai = 1,
-	etat = index;
-    if (cosi < 0) { cosi = -cosi; vec_dup(norm, tnorm); } else { ft_swap(&etai, &etat, sizeof(double)); v_multi(norm, -1, tnorm); }
-    double eta = etai / etat;
-    double k = 1 - eta * eta * (1 - cosi * cosi); 
-    k < 0 ? FILLVEC(ret, 0, 0, 0, 0) : v_add(v_multi(inc, eta, ret), v_multi(tnorm, eta * cosi - sqrtf(k), tnorm), ret)[0];
+	var[3] = ft_clamp(1, -1, dot(inc, norm));
+	var[2] = index;
+	refract_helper(norm, tnorm, var);
+    var[5] < 0 ? FILLVEC(ret, 0, 0, 0, 0) : v_add(v_multi(inc, var[4], ret),
+		v_multi(tnorm, var[4] * var[3] - sqrtf(var[5]), tnorm), ret)[0];
 	return (ret);
 }
